d4173de9ad779eed5209f1f6caeeda297f1d7ac0
akmadnudin <akmadnudin@indobara.co.id>
2026-05-19 16:55:37 +0700

    default themes light

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -90,10 +90,11 @@ const config: Config = {
   themeConfig: {
     image: 'img/docusaurus-social-card.jpg',
 
-    colorMode: {
-      respectPrefersColorScheme: true,
-    },
-
+     colorMode: {
+    defaultMode: 'light',
+    disableSwitch: true,
+    respectPrefersColorScheme: false,
+  },
     navbar: {
       title: 'My Books',
 
